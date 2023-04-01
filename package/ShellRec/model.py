c4ee80701bab9850cfc4fb5771d4a85423b3b6fe
YunyiShen <yshen99@wisc.edu>
2023-03-31 21:18:30 -0400

    train last attention layer

--- package/ShellRec/model.py
+++ package/ShellRec/model.py
@@ -30,6 +30,34 @@ class TurtleDiff(nn.Module):
         x = self.fc2(x)
         return x
 
+class TurtleDiffConv(nn.Module):
+    def __init__(self, hidden = 100):
+        super(TurtleDiffConv, self).__init__()
+        self.backbone = timm.create_model('resnet50', 
+                                          pretrained=True,
+                                          num_classes=0)
+        self.backbone_name = 'resnet50-conv'
+
+        self.fc = nn.Linear(self.backbone.num_features, hidden)
+        self.fc2 = nn.Linear(hidden, 2)
+
+        # Freeze the parameters of the backbone if use pretrained
+        for name, param in self.backbone.named_parameters():
+            if name.startswith("layer4.2"):
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+    
+    def forward(self, x1, x2):
+        x1 = self.backbone(x1) 
+        x2 = self.backbone(x2)
+        # difference between the two image embeddings, one way to make sure symmetry
+        x = torch.abs(x1 - x2) 
+        x = self.fc(x)
+        x = nn.ReLU()(x)
+        x = self.fc2(x)
+        return x
+
 class TurtleDiffPool(nn.Module):
     def __init__(self, backbone, hidden = 100,pretrained = True):
         super(TurtleDiffPool, self).__init__()
@@ -66,7 +94,6 @@ class TurtleDiffPool(nn.Module):
         
         return x
     
-
 class TurtleDiffConvPool(nn.Module):
     def __init__(self, hidden = 100):
         super(TurtleDiffConvPool, self).__init__()
@@ -107,20 +134,21 @@ class TurtleDiffConvPool(nn.Module):
         
         return x
 
-class TurtleDiffConv(nn.Module):
+class TurtleDiffAttnPool(nn.Module):
     def __init__(self, hidden = 100):
-        super(TurtleDiffConv, self).__init__()
-        self.backbone = timm.create_model('resnet50', 
+        super(TurtleDiffAttnPool, self).__init__()
+        
+        self.backbone = timm.create_model('vit_base_patch16_384', 
                                           pretrained=True,
                                           num_classes=0)
-        self.backbone_name = 'resnet50-conv'
-
+        
+        self.backbone_name = 'vit_base_patch16_384-attn-pool'
+        
         self.fc = nn.Linear(self.backbone.num_features, hidden)
         self.fc2 = nn.Linear(hidden, 2)
 
-        # Freeze the parameters of the backbone if use pretrained
         for name, param in self.backbone.named_parameters():
-            if name.startswith("layer4.2"):
+            if name.startswith("blocks.11.attn"):
                 param.requires_grad = True
             else:
                 param.requires_grad = False
@@ -128,9 +156,21 @@ class TurtleDiffConv(nn.Module):
     def forward(self, x1, x2):
         x1 = self.backbone(x1) 
         x2 = self.backbone(x2)
-        # difference between the two image embeddings, one way to make sure symmetry
-        x = torch.abs(x1 - x2) 
+        # get two way difference, then pool them after nonlinearity
+        
+        x1p = x1 - x2
+        x2p = x2 - x1
+        x1p = self.fc(x1p)
+        x1p = nn.ReLU()(x1p)
+        x2p = self.fc(x2p)
+        x2p = nn.ReLU()(x2p)
+        x = (x1p+x2p)/2
+        '''
+        x = x1 - x2
         x = self.fc(x)
-        x = nn.ReLU()(x)
+        x = torch.abs(x) # equivalent to the above
+        '''
         x = self.fc2(x)
+        
         return x
+
