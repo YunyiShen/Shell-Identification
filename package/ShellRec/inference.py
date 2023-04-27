391b74fb4425a67db87c258f9e625fc1584733df
YunyiShen <yshen99@wisc.edu>
2023-04-27 11:14:53 -0400

    fix bug

--- package/ShellRec/inference.py
+++ package/ShellRec/inference.py
@@ -7,12 +7,13 @@ import torch.nn as nn
 def inference(model, data_loader, device):
     results_list = []
     model.to(device)
+    m = nn.Softmax(dim=1)
     with torch.no_grad():
             for images1, images2 in tqdm(data_loader):
                 images1, images2 = images1.to(device), \
                                    images2.to(device)
                 outputs = model(images1, images2)
-                predicted = nn.Softmax(outputs.data, 1)[:,1] # probability of being a match
+                predicted = m(outputs.data)[:,1] # probability of being a match
                 results_list.append(predicted.cpu().numpy().tolist())
     return results_list
 
@@ -20,12 +21,13 @@ def test_model(model, data_loader, device):
     results_list = []
     label_list = []
     model.to(device)
+    m = nn.Softmax(dim=1)
     with torch.no_grad():
             for images1, images2, labels in tqdm(data_loader):
                 images1, images2 = images1.to(device), \
                                    images2.to(device)
                 outputs = model(images1, images2)
-                predicted = nn.Softmax(outputs.data, 1)[:,1] # probability of being a match
+                predicted = m(outputs.data)[:,1] # probability of being a match
                 label_list.append(labels.cpu().numpy().tolist())
                 results_list.append(predicted.cpu().numpy().tolist())
     return {'predicted': results_list, 'labels': label_list}
